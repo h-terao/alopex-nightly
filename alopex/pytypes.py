e07c915aaef4e0ceed4806a05e60b7b25f45f4d9
h-terao <eresh@outlook.jp>
2023-06-23 05:02:30 +0900

    update.

--- alopex/pytypes.py
+++ alopex/pytypes.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 """Types and type utilities."""
-from alopex._src.get_dtypes import DTypes
+from alopex._src.dtypes import DTypes
 from alopex._src.dynamic_scale import DynamicScale
 from alopex._src.loop import TrainState
 from alopex._src.loop import TrainFun
