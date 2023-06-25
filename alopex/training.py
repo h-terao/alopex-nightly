873cd168e8455a62f40e1d6bdf6ebeff2db57bc5
h-terao <eresh@outlook.jp>
2023-06-26 01:25:22 +0900

    remove accumulate_grads.

--- alopex/training.py
+++ alopex/training.py
@@ -1,7 +1,5 @@
 # flake8: noqa
 from alopex._src.loop import train_loop
 from alopex._src.loop import eval_loop
-from alopex._src.accumulate_grads import accumulate_grads
 from alopex._src.load_variables import load_variables
-
 from alopex._src.dynamic_scale import DynamicScale
